edaca4a035d34400ebacd7769868b4661c1489c1
Surya Chandra <suryavijetha123@gmail.com>
2025-03-20 13:30:42 +0530

    updated DB Password

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 // When you declare a function with async, it automatically returns a promise, even if you don’t use a return statement. The promise returned by an async function is resolved with the return value of the function, or rejected with an exception thrown from the function.
 const connectDB=async ()=>{
-    await mongoose.connect("mongodb+srv://suryachandra379:G898OjZiVu6DiqCN@namastedev.hf7i0.mongodb.net/devTinder")
+    await mongoose.connect("mongodb+srv://suryachandra379:agGOSqdluoA9MHio@namastedev.hf7i0.mongodb.net/devTinder")
 };
 module.exports=connectDB;
